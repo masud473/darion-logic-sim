4a3112dad27e7eb6c0d141cf69120a08cdbc4e92
Farhan <mdfarhanmasud584@gmail.com>
2025-12-26 00:27:23 +0600

    Backend + CLI version-2.0

--- Farhan/Backend.py
+++ Farhan/Backend.py
@@ -1,103 +1,70 @@
-
-from encodings.punycode import T
-
-
-objlist={}
-complist=[]
-varlist=[]
-
-
 class Signal:
     # default signals that exist indepdently
-    def __init__(self,value):
-        self.value=value
-        self.parents=[]
-    def output(self):
-        return self.value
-    
-objlist['0']=Signal(0)
-objlist['1']=Signal(1)
-objlist['-1']=Signal(-1)
-
+    def __init__(self,circuit,value):
+        self.circuit=circuit
+        self.parents=set()
+        self.output=value
 
 class Gate:   
 
-    def __init__(self):
+    def __init__(self,circuit):
         # gate's children or inputs
-        self.children=[]
-        # parents of the gate
-        self.parents=[]
+        self.circuit=circuit
+        self.children=[set(),set()]
+        self.parents=set()
         # input limit
         self.inputlimit=2
+        #default output
+        self.output=0
         # each gate will have it's own unique id
         self.code=''
-    # sets number of children
-    def setlimits(self,l):
-        self.inputlimit=l
 
-    # connects gates
-    def connect(self,child):
-        if self.code in varlist:
-            self.children[0]=child
-        elif len(self.children)<self.inputlimit:
-            self.children.append(child)
-            objlist[child].parents.append(self.code)
+    def turnon(self):
+        return len(self.children[0])+len(self.children[1])>=self.inputlimit
 
-    # deletes parent from the parent list
-    def disconnect(self,node):    
-        # check if node is a parent or a child
-        # then delete accordingly
-        if node in self.parents:
-            objlist[node].children.remove(self.code)
-            self.parents.remove(node)
-        elif node in self.children:
-            objlist[node].parents.remove(self.code)
-            self.children.remove(node)
-        else:
-            print('Not Connected')
-
-    # checks if there are enough desired children
-    def output_check(self):
-        return len(self.children)==self.inputlimit
-    
-    def output(self):
+    def process(self):
         pass
-    
+
     # gives output in T or F
     def display_output(self):
-        out=self.output()
-        return 'T' if out else 'F'
-
-
-
+        if self.turnon()==False:
+            return 'OFF'
+        elif self.output==0:
+            return 'F'
+        else:
+            return 'T'
 
 
 class Variable(Gate):
     # this can be both an input or output(bulb)
     rank=0
-    def __init__(self):
-        super().__init__()        
+    def __init__(self,circuit):
+        super().__init__(circuit)          
         self.inputlimit=1
         self.code=chr(ord('A') + Variable.rank)
         Variable.rank+=1
-        self.children.append('0')
+        self.children[0].add('0')
     # no changing the limit
     def setlimits(self):
         pass
-    def output(self):
-        if self.output_check()==False:
-                return -1        
-        child =objlist[self.children[0]].output()
-        if child==-1:
-            return -1
+    def process(self):
+        out=self.output
+        if len(self.children[0]):
+            out=0
+        elif len(self.children[1]):
+            out=1
         else:
-            return child
-    
+            out=0
+        prev=self.output
+        self.output=out
+        # may need to update when error
+        self.circuit.update(self.code,prev)
+
         
 class NOT(Gate):
     rank=0
-    def __init__(self):
-        super().__init__()        
+    def __init__(self,circuit):
+        super().__init__(circuit)        
         self.inputlimit=1
         NOT.rank+=1
         self.code='NOT-'+str(NOT.rank)
@@ -105,155 +72,337 @@ class NOT(Gate):
     # no changing the limit
     def setlimits(self):
         pass
-    def output(self):
-        if self.output_check()==False:
-            return -1
-        
-        child=objlist[self.children[0]].output()
-        if child==-1:
-            return -1
+    def process(self):
+        out=self.output
+        if len(self.children[0]):
+            out=1
+        elif len(self.children[1]):
+            out=0
         else:
-            return not child
+            out=0
+        prev=self.output
+        self.output=out
+        self.circuit.update(self.code,prev)
         
 class AND(Gate):
 
     rank=0
-    def __init__(self):
-        super().__init__()        
+    def __init__(self,circuit):
+        super().__init__(circuit)       
         AND.rank+=1
         self.code='AND-'+str(AND.rank)
-    def output(self):
-        if self.output_check()==False:
-            return -1
-        # iterate through the children/inputs
-        x=objlist[self.children[0]].output()
-        for i in range(1,self.inputlimit):
-            x= x and objlist[self.children[i]].output()
-        return x
-    
+    def process(self):
+        out=self.output
+        if len(self.children[0]):
+            out=0
+        elif len(self.children[1]):
+            out=1
+        else: 
+            out=0
+        prev=self.output
+        self.output=out
+        self.circuit.update(self.code,prev)
 
-
-class NAND(AND):
+        
+class NAND(Gate):
     rank=0
     # basically a not + and gate
-    def __init__(self):
-        super().__init__()
+    def __init__(self,circuit):
+        super().__init__(circuit)   
         NAND.rank+=1
         self.code='NAND-'+str(NAND.rank)
-        AND.rank-=1
-    def output(self):
-        return not super().output()
+    def process(self):
+        out=self.output
+        if len(self.children[0]):
+            out=1
+        elif len(self.children[1]):
+            out=0
+        else: 
+            out=0
+        prev=self.output
+        self.output=out
+        self.circuit.update(self.code,prev)
 
 class OR(Gate):
     rank=0
-    def __init__(self):
-        super().__init__()        
+    def __init__(self,circuit):
+        super().__init__(circuit)         
         OR.rank+=1
         self.code='OR-'+str(OR.rank)
-    def output(self):
-        if self.output_check()==False:
-            return -1
-        x=objlist[self.children[0]].output()
-        for i in range(1,self.inputlimit):
-            x= x or objlist[self.children[i]].output()
-        return x
+    def process(self):
+        out=self.output
+        if len(self.children[1]):
+            out=1
+        else: 
+            out=0
+        prev=self.output
+        self.output=out
+        self.circuit.update(self.code,prev)
 
-class NOR(OR):
+class NOR(Gate):
     rank=0
-    def __init__(self):
-        super().__init__()
+    def __init__(self,circuit):
+        super().__init__(circuit)   
         NOR.rank+=1
         self.code='NOR-'+str(NOR.rank)
-        OR.rank-=1
-    def output(self):
-        return not super().output()
+    def process(self):
+        out=self.output
+        if len(self.children[1]):
+            out=0
+        elif len(self.children[0]):
+            out=1
+        else: 
+            out=0
+        # output needs to be updated first
+        prev=self.output
+        self.output=out
+        self.circuit.update(self.code,prev)
 
 class XOR(Gate):
     rank=0
-    def __init__(self):
-        super().__init__()        
+    def __init__(self,circuit):
+        super().__init__(circuit)       
         XOR.rank+=1
         self.code='XOR-'+str(XOR.rank)
-    def output(self):
-        if self.output_check()==False:
-            return -1
-        x=objlist[self.children[0]].output()
-        for i in range(1,self.inputlimit):
-            x= x ^ objlist[self.children[i]].output()
-        return x
+    def process(self):
+        out=int(len(self.children[1])%2)
+        prev=self.output
+        self.output=out
+        self.circuit.update(self.code,prev)
 
-class XNOR(XOR):
+class XNOR(Gate):
     rank=0
-    def __init__(self):
-        super().__init__()
+    def __init__(self,circuit):
+        super().__init__(circuit)   
         XNOR.rank+=1
         self.code='XNOR-'+str(XNOR.rank)
-        XOR.rank-=1
-    def output(self):
-        return not super().output()
+    def process(self):
+        out=int(len(self.children[1])%2==0)
+        prev=self.output
+        self.output=out
+        self.circuit.update(self.code,prev)
 
 
 # inventory
-def listComponent():
-    for i in range(len(complist)):
-        print(f'{i}. {complist[i]}')
-
-def listVar():
-    for i in range(len(varlist)):
-        print(f'{i}. {varlist[i]}')
-
-def addComponent():
-    print("Choose a gate to add to the circuit:")
-    print("1. NOT")
-    print("2. AND")
-    print("3. NAND")
-    print("4. OR")
-    print("5. NOR")  
-    print("6. XOR")
-    print("7. XNOR")
-    print("8. Variable")     
-
-    choice = input("Enter your choice: ").split()
-
-    for i in choice:
-        if i == '1':
-            gt = NOT()
-        elif i == '2':
-            gt = AND()
-        elif i == '3':
-            gt = NAND()
-        elif i == '4':
-            gt = OR()
-        elif i == '5':
-            gt = NOR()                
-        elif i == '6':
-            gt = XOR()
-        elif i == '7':
-            gt = XNOR()
-        elif i=='8':
-            gt=Variable()
-            varlist.append(gt.code)
-        objlist[gt.code]=gt
-        complist.append(gt.code)
-
-def deleteComponent(gate):
-    gate_obj=objlist[gate]
-    for child in gate_obj.children:
-        objlist[child].parents.remove(gate)
-    for parent in gate_obj.parents:
-        objlist[parent].children.remove(gate)
-    del objlist[gate]
-    
-# wiring
-
-def connect(gate,comp):
-    objlist[gate].connect(comp)
-def disconnect(gate,comp):
-    objlist[gate].disconnect(comp)
+class Circuit:
+    def __init__(self):
+        self.objlist={}
+        self.complist=[]
+        self.varlist=[]
+        self.probelist=[]
+        self.circuit_breaker={}
+        self.objlist['0']=Signal(self,0)
+        self.objlist['1']=Signal(self,1)
 
 
-# Result 
-def output(gate):
-    print(objlist[gate].display_output())
+    def listComponent(self):
+        for i in range(len(self.complist)):
+            print(f'{i}. {self.complist[i]}')
+
+    def listVar(self):
+        for i in range(len(self.varlist)):
+            print(f'{i}. {self.varlist[i]}')
+
+    def addComponent(self):
+        print("Choose a gate to add to the circuit:")
+        print("1. NOT")
+        print("2. AND")
+        print("3. NAND")
+        print("4. OR")
+        print("5. NOR")  
+        print("6. XOR")
+        print("7. XNOR")
+        print("8. Variable")     
+
+        choice = input("Enter your choice: ").split()
+
+        for i in choice:
+            if i == '1':
+                gt = NOT(self)
+            elif i == '2':
+                gt = AND(self)
+            elif i == '3':
+                gt = NAND(self)
+            elif i == '4':
+                gt = OR(self)
+            elif i == '5':
+                gt = NOR(self)                
+            elif i == '6':
+                gt = XOR(self)
+            elif i == '7':
+                gt = XNOR(self)
+            elif i=='8':
+                gt=Variable(self)
+                self.varlist.append(gt.code)
+            self.objlist[gt.code]=gt
+            self.complist.append(gt.code)
+            self.circuit_breaker[gt.code]=-1
+
+    def deleteComponent(self,gate):
+        gate_obj=self.objlist[gate]
+        parent_list=list(gate_obj.parents)
+        for parent in parent_list:
+            self.disconnect_gates(parent,gate)
+        for child in gate_obj.children[0]:
+            self.objlist[child].parents.discard(gate)
+        for child in gate_obj.children[1]:
+            self.objlist[child].parents.discard(gate)        
+        del self.objlist[gate]
+      
+        
+    # wiring
+    def fix(self,parent,child):
+        parent_obj=self.objlist[parent]
+        child_obj=self.objlist[child]
+
+        child_obj.parents.discard(parent)
+        parent_obj.children[0].discard(child)
+        parent_obj.children[1].discard(child)
+        
+        if isinstance(parent_obj,Variable):
+            self.connect(parent,'0')
+        else:
+            parent_obj.process()
+
+    def update(self,gate,prev):
+        gate_obj=self.objlist[gate]
+        out=gate_obj.output
+        if self.circuit_breaker[gate]==-1:
+            self.circuit_breaker[gate]=out
+            if gate_obj.turnon() and prev!=out:
+                for parent in gate_obj.parents:
+                    self.connect(parent,gate)
+                    if self.objlist[parent].output==-1:
+                        gate_obj.output=-1
+                        break
+            self.circuit_breaker[gate]=-1
+        elif self.circuit_breaker[gate]==out:
+            return
+        else:
+            print('Loop Detected')
+            gate_obj.output=-1
+
+    def connect(self,gate,child):
+        gate_obj=self.objlist[gate]
+        child_obj=self.objlist[child]
+        # check for variable or probe
+        if isinstance(gate_obj,Variable):
+            if isinstance(child_obj,Signal):
+                # variable has a 0/1 input so it's not a probe
+                if gate in self.probelist:
+                    self.probelist.remove(gate)
+                    self.varlist.append(gate)
+            else:
+                # a probe has a gate input
+                if gate in self.varlist:
+                    self.varlist.remove(gate)
+                    self.probelist.append(gate)
+        val=child_obj.output
+        # connect child to self
+        if child in gate_obj.children[val]:
+            return
+        else:
+            if isinstance(gate_obj,Variable) or isinstance(gate_obj,NOT):
+                gate_obj.children[val].clear()
+            gate_obj.children[val].add(child)
+        if child in gate_obj.children[val^1]:
+            gate_obj.children[val^1].discard(child)
+        if isinstance(gate_obj,Variable) or isinstance(gate_obj,NOT):
+            gate_obj.children[val^1].clear()
+
+        # connect itself to children
+        if isinstance(child_obj,Signal)==False and gate_obj not in child_obj.parents:
+            child_obj.parents.add(gate)
+        gate_obj.process()
 
 
+    # disconnects parent & child
+    def disconnect_gates(self,parent,child):
+        parent_obj=self.objlist[parent]
+        child_obj=self.objlist[child]
+        parent_obj.children[child_obj.output].discard(child)
+        parent_obj.process()
+        child_obj.parents.discard(parent)
+
+
+    def disconnect(self,gate1,gate2):
+        
+        gate1_obj=self.objlist[gate1]
+        gate2_obj=self.objlist[gate2]
+
+        # check for parenthood and delete with function
+        if gate1 in gate2_obj.parents:
+            self.disconnect_gates(gate1,gate2)            
+        elif gate2 in gate1_obj.parents:
+            self.disconnect_gates(gate2,gate1)
+        else:
+            print(f'{gate1} & {gate2} not connected')
+
+
+    # Result 
+    def output(self,gate):
+        print(f'{gate} output is {self.objlist[gate].display_output()}')
+
+
+    # Truth Table
+    def truthTable(self,gate):
+        if len(self.varlist)==0:
+            print('No variable for toggling')
+            return
+        if gate in self.varlist:
+            print(f'{gate} is a variable not a gate or a probe')
+        elif gate in self.complist :
+            bits=1<<len(self.varlist)
+            for i in self.varlist:
+                print(i,end=' ')
+            print(gate)
+            for i in range(bits):
+                for j in range(len(self.varlist)):
+                    var=self.varlist[j]
+                    bitpoint=int((i & (1<<(len(self.varlist)-j-1)))!=0)
+                    self.connect(var,str(bitpoint))
+                    print('T' if self.objlist[var].output else 'F',end=' ')
+                print('T' if self.objlist[gate].output else 'F')
+
+
+    # diagnosis: this menu is AI generated and it's not the main part of code just to check errors in CLI mode
+    def diagnose(self):
+        print("--- Component Diagnosis ---")
+        
+        # Define columns dynamically (easy to add/remove in the future)
+        columns = [
+            ("Component", 12),
+            ("Input-0", 22),
+            ("Input-1", 22),
+            ("Parents (Outputs to)", 25),
+            ("State", 10)
+        ]
+        
+        # Calculate total width for separator line
+        total_width = sum(width for _, width in columns)
+        
+        # Header row
+        header_format = "".join(f"{{:<{width}}}" for _, width in columns)
+        header_names = [name for name, _ in columns]
+        print(header_format.format(*header_names))
+        print("-" * total_width)
+        
+        # Data rows
+        row_format = header_format  # Same alignment and widths
+        
+        for comp_code in self.complist:
+            comp_obj = self.objlist[comp_code]
+            
+            # Inputs (children)
+            children_0 = ", ".join(sorted(comp_obj.children[0])) if comp_obj.children[0] else "None"
+            children_1 = ", ".join(sorted(comp_obj.children[1])) if comp_obj.children[1] else "None"
+            
+            # Outputs (parents)
+            parents = ", ".join(sorted(comp_obj.parents)) if comp_obj.parents else "None"
+            
+            # State
+            state = comp_obj.display_output()
+            
+            print(row_format.format(comp_code, children_0, children_1, parents, state))
+        
+        print("-" * total_width)
--- Farhan/CLI.py
+++ Farhan/CLI.py
@@ -1,8 +1,17 @@
-import Backend as base
+from Backend import Circuit
 from readchar import readkey,key
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# Usage
+base=Circuit()
+
 def menu():
     while True:
-        print("\n--- Circuit Simulator Menu ---")
+        clear_screen()
+        print("--- Circuit Simulator Menu ---")
         print("1. Add Component")
         print("2. List Components")
         print("3. Connect Components")
@@ -10,35 +19,47 @@ def menu():
         print("5. Delete Components")
         print("6. Set Input Variable Value")
         print("7. Show Output of a Component")
-        print("8. Exit")
+        print("8. Show Truth Table of a Component")
+        print("9. Diagnose Components")
 
-        print("Enter your choice: ",end='')
+        print("Enter your choice or press ESC to quit: ",end='')
         choice = readkey()
 
+        print()
+        clear_screen()
         if choice == '1':
             base.addComponent()
 
         elif choice == '2':
             base.listComponent()
-
+            input('Press any key to continue....')
         elif choice == '3':
             base.listComponent()
             gate_code = input("Enter the serial of the gate you want to connect components: ")
+            if gate_code=='':
+                continue
             gate_code=base.complist[int(gate_code)]
             childlist = list(map(int,input("Enter the serial of the component to connect to: ").split()))
             for child in childlist:
                 base.connect(gate_code, base.complist[child])
-                print(f"Connected {base.complist[child]} to {gate_code}.")
-
+                if base.objlist[gate_code].output==-1:
+                    base.fix(gate_code,base.complist[child])
+                    print(f"Cannot connect {base.complist[child]} & {gate_code} due to deadlock")
+                else:    
+                    print(f"Connected {base.complist[child]} to {gate_code}.")
+            input('Press any key to continue....')
         elif choice == '4':
             base.listComponent()
             gate_code = input("Enter the serial of the gate you want to disconnect components: ")
+            if gate_code=='':
+                continue
             gate_code=base.complist[int(gate_code)]
+
             childlist = list(map(int,input("Enter the serial of the component to disconnect to: ").split()))
             for child in childlist:
                 base.disconnect(gate_code, base.complist[child])
                 print(f"Disconnected {base.complist[child]} & {gate_code}.")
-
+            input('Press any key to continue....')
         elif choice == '5':
             base.listComponent()
             gatelist = list(map(int,input("Enter the serial of the components you want to delete: ").split()))
@@ -48,23 +69,41 @@ def menu():
                 del base.complist[gate]
         elif choice == '6':
             base.listVar()
-            var = input("Enter the code of the variable to set : ")
+            var = input("Enter the serial of the variable to set : ")
+            if var=='':
+                continue
+            var=base.varlist[int(var)]
             if var in base.varlist:
                 value = input("Enter the value (0 or 1): ")
                 if value in ['0', '1']:
                     base.connect(var, value)
                     print(f"Variable {var} set to {value}.")
                 else:
-                    print("Invalid value. Please enter 0 or 1.")
-
+                    print("Invalid value. Please try again")
+            input('Press any key to continue....')
         elif choice == '7':
             base.listComponent()
             gate_code = input("Enter the serial of the gate you want to see output of: ")
+            if gate_code=='':
+                continue
             gate_code=base.complist[int(gate_code)]
             base.output(gate_code)
-           
+            input('Press any key to continue....')
+        elif choice == '8':
+            base.listComponent()
+            gate_code = input("Enter the serial of the gate you want to see Truth Table of: ")
+            if gate_code=='':
+                continue
+            gate_code=base.complist[int(gate_code)]
+            base.truthTable(gate_code)
+            input('Press any key to continue....')
+        elif choice == '9':
+            base.diagnose()
+            input('Press any key to continue....')
         elif choice == key.ESC:
-            print("\nExiting Circuit Simulator.")
+            print("Exiting Circuit Simulator......")
+            input('Press any key to continue....')
+            clear_screen()
             break
         else:
             print("Invalid choice. Please try again.")
